2e00b13793806a2beff35486d1fef3a2fec539e5
julianadelfino <dsjulianadelfino447@gmail.com>
2024-10-01 15:48:48 -0300

    criei o css

--- cadastro.php
+++ cadastro.php
@@ -3,6 +3,9 @@
 
 require_once 'includes/config.php';
 
+$mensagem_sucesso= "";
+$mensagem_erro= "";
+
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $nome = $_POST['nome'];
@@ -33,6 +36,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
       $mensagem_erro = "Erro ao cadastrar" . $conn->error;
     }
   }
+  $stmt->close();
+$conn->close();
 }
 
 
@@ -46,23 +51,41 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/cadastro.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <title>Document</title>
 </head>
 
 
 <body>
   <div>
-    <form action="" method="POST">
+    <form class="alinhar"action="" method="POST">
+      <div class="login">
       <h2>Cadastro de usuarios</h2>
+      </div>
+      <div class="email">
       <label for="nome">Nome:</label>
       <input type="text" id="nome" name="nome" required><br>
+      </div>
+      <div class="email">
       <label for="email">Email:</label>
       <input type="text" id="email" name="email" required><br>
-      <label for="senha">Senha:</label>
-      <input type="password" id="senha" name="senha" required><br>
+      </div>
+     <div class="email">
+     <label for="senha">Senha:</label>
+     <input type="password" id="senha" name="senha" required><br>
+     </div>
+      
 
 
-      <input type="submit" value="Cadastrar">
+     <div class="cadastro">
+     <input type="submit" value="Cadastrar">
+     </div>
     </form>
   </div>
 </body>
--- index.php
+++ index.php
@@ -3,11 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 
 <body>
-        <a href="./cadastro.php">Ir para cadastro</a>
+       
+  <div>
+    <form class="alinhar"action="" method="POST">
+      <div class="login">
+      <h2>Cadastro de usuários</h2>
+      </div>
+   
+   <div class="junta">
+   <div class="email">
+     <label for="email">Email:</label>
+     <input type="text" id="email" name="email" required><br>
+     </div> 
+      <div class="senha">
+      <label for="senha">Senha:</label>
+      <input type="password" id="senha" name="senha" required><br>
+
+      </div>
+   </div>
+    
+   
+
+     <div class="cadastro">
+     <input type="submit" value="Cadastrar">
+     </div> 
+    <div class="login">
+    <p>Ja tem conta? <a href="cadastro.php">Ir pra login</a></p>
+    </div>
+    
+      
+    </form>
+  </div>
+
+
 </body>
 
 </html>
